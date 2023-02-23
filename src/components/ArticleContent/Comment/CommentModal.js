3bf08de566d29d842e173ffb1eb8ecade85195dd
zhiqiao.yang <zhiqiao.yang@thoughtworks.com>
2023-02-23 16:32:36 +0800

    ZQ-5 Fix: Modify style codes

--- src/components/ArticleContent/Comment/CommentModal.js
+++ src/components/ArticleContent/Comment/CommentModal.js
@@ -39,7 +39,7 @@ export const CommentModal = (props) => {
         {showComment}
         <Form className="input-box" reply>
           <Form.TextArea
-            style={{width:"1000px"}}
+            style={{width: "1000px"}}
             placeholder="Type something here…"
             onChange={inputComment}
             required={isCommentBoxEmpty && "required"}
